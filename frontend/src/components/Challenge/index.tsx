f091dc59526242213d5d30b487c82e6257b532a0
DiegoVP66 <dvpdev33@gmail.com>
2022-05-14 15:59:39 -0300

    Challenge component style

--- frontend/src/components/Challenge/index.tsx
+++ frontend/src/components/Challenge/index.tsx
@@ -2,8 +2,38 @@ import "./styles.css";
 
 const Challenge = () => {
   return (
-    <div>
-      <h1>Challenge</h1>
+    <div className="container challenge-table-container base-card">
+      <h4 className="mt-4">
+        TQI Bootcamp <span>125hrs</span>
+      </h4>
+      <table className="challenge-table">
+        <thead>
+          <tr>
+            <th>Course</th>
+            <th>Instructor</th>
+            <th>Hours</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Introduction to programming and computational thinking</td>
+            <td>Juliana Mascarenhas</td>
+            <td>5</td>
+          </tr>
+          <tr>
+            <td>Introduction to Git and GitHub</td>
+            <td>Otávio Reis</td>
+            <td>5</td>
+          </tr>
+          <tr>
+            <td>
+              Working with Collections <span>(JAVA)</span>{" "}
+            </td>
+            <td>Camila Cavalcante</td>
+            <td>6</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 };
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,11 +1,26 @@
 import Challenge from "components/Challenge";
+import Rocket from "assets/img/rocket.svg";
+
+import "./styles.css";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
-      <div>
-        <Challenge />
+    <div className="home-container" id="home">
+      <div className="container mt-4 home-content base-card">
+        <h1>GitHub Challenge</h1>
+        <span> "My First Git Repository"</span>
+        <p>
+          The challenge is to create a GitHub repository with all the materials
+          from the bootcamp I am currently studying.
+        </p>
+      </div>
+      <div className="home-content-container">
+        <div className="home-challenge-container" id="challenge">
+          <Challenge />
+        </div>
+        <div className="home-img-container">
+          <img src={Rocket} alt="" />
+        </div>
       </div>
     </div>
   );
