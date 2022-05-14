9d7231ada3a2c6809e365349e321dad5f5d1a664
DiegoVP66 <dvpdev33@gmail.com>
2022-05-13 21:11:12 -0300

    Backend created

---
+++ backend/src/main/java/com/diegoTQIBootcamp/TQI/TqiApplication.java
@@ -0,0 +1,13 @@
+package com.diegoTQIBootcamp.TQI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TqiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TqiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/diegoTQIBootcamp/TQI/TqiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diegoTQIBootcamp.TQI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TqiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
