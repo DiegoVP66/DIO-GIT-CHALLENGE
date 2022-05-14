788579f05e906d56875644713f1838058f8edb7b
DiegoVP66 <dvpdev33@gmail.com>
2022-05-14 13:39:57 -0300

    Challenge component created

---
+++ frontend/src/components/Challenge/index.tsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+const Challenge = () => {
+  return (
+    <div>
+      <h1>Challenge</h1>
+    </div>
+  );
+};
+
+export default Challenge;
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,7 +1,12 @@
+import Challenge from "components/Challenge";
+
 const Home = () => {
   return (
     <div>
       <h1>Home</h1>
+      <div>
+        <Challenge />
+      </div>
     </div>
   );
 };
