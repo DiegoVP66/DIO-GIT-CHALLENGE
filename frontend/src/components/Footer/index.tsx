d9da67848e2c3b3da7fa23d7dd23f72296001965
DiegoVP66 <dvpdev33@gmail.com>
2022-05-14 21:47:08 -0300

    Static frontend ok

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -9,10 +9,18 @@ const Footer = () => {
       <div className="fotter-content">
         <h4>Diego Vicente Pereira</h4>
         <div className="footer-img">
-          <a href="https://github.com/DiegoVP66">
+          <a
+            href="https://github.com/DiegoVP66"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <img src={Git} alt="" />
           </a>
-          <a href="https://www.linkedin.com/in/diego-vicente-pereira-212647212/">
+          <a
+            href="https://www.linkedin.com/in/diego-vicente-pereira-212647212/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <img src={In} alt="" />
           </a>
         </div>
